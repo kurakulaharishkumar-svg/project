ec69ccc151390065633e07fadbdc045b39e615d4
kurakulaharishkumar-svg <kurakulaharishkumar@gmail.com>
2025-08-16 21:07:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 print("hello harish")
 print("hello jaya")
 print("this is git hub")
-print("great")
-<<<<<<< HEAD
-print("hackaton 2")
-=======
-print("hackthon")
->>>>>>> 6af88261cd4b43f195abbdd1272db201ac05b1ef
+print("this is practice")
